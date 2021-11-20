5a262c85bf45e19c081103d9f1bad324544300bd
Al2206 <77494086+Al2206@users.noreply.github.com>
2021-11-21 02:24:53 +0300

    Update Exercise7.java

--- src/HomeWork/Exercise7.java
+++ src/HomeWork/Exercise7.java
@@ -1,23 +1,33 @@
 // 7) Сформировать двумерный массив размером 10*10.
 // Найти минимальный и максимальный элемент на экран
 package HomeWork;
-import java.util.Arrays;
+
 public class Exercise7 {
     public static void main(String[] args) {
         int[][] array = new int[10][10];
-        int min = 1000, max = 0;
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length; j++) {
-                array[i][j] = (int) ((Math.random() * 1000) + 1);
-                if (array[i][j] < min) {
-                    min = array[i][j];
-                } else if (array[i][j] > max) {
-                    max = array[i][j];
-                }
+                array[i][j] = (int) ((Math.random() * 999) + 1);
                 System.out.print(array[i][j] + "\t");
             }
             System.out.println();
         }
-        System.out.println("MIN = " + min + "\n" + "MAX = " + max);
+        int max = array[0][0];
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (array[i][j] > max) {
+                    max = array[i][j];
+                }
+            }
+            int min = array[0][0];
+            for (i = 0; i < array.length; i++) {
+                for (int j = 0; j < array.length; j++) {
+                    if (array[i][j] < min) {
+                        min = array[i][j];
+                    }
+                    System.out.println("MIN = " + min + "\n" + "MAX = " + max);
+                }
+            }
+        }
     }
 }
