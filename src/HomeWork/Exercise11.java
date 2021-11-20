2100b43b5017317652e819320e48aafc374790ba
Al2206 <77494086+Al2206@users.noreply.github.com>
2021-11-21 00:48:20 +0300

    Update Exercise11.java

--- src/HomeWork/Exercise11.java
+++ src/HomeWork/Exercise11.java
@@ -7,11 +7,20 @@ public class Exercise11 {
         for (int i = 0; i < array.length; i++) {
             for (int j = 0; j < array.length; j++) {
                 array[i][j] = (int) ((Math.random() * 100) + 1);
-                if (i == j);
-
                 System.out.print(array[i][j] + "\t");
+            }
+            System.out.println();
+        }
+        System.out.print("\n");
+        for (int i = 0; i < array.length; i++) {
+            for (int j = 0; j < array.length; j++) {
+                if (i + j >= array.length) array[i][j] = 0;
+                {
+                    System.out.print(array[i][j] + "\t");
+                }
             }
             System.out.println();
         }
     }
 }
+// Хотел, что бы вместо " 0 " вообще ни чего не печатало... Не получилось..
