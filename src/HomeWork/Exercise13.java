6d6b60e4ef32335fb8b18f710196d064eab2fcf9
Al2206 <77494086+Al2206@users.noreply.github.com>
2021-11-21 00:51:32 +0300

    Update Exercise13.java

--- src/HomeWork/Exercise13.java
+++ src/HomeWork/Exercise13.java
@@ -2,7 +2,9 @@
 // т. е. все элементы одного столбца поставить на соответствующие им позиции другого,
 // а его элементы второго переместить в первый. Номера столбцов вводит пользователь с клавиатуры.
 package HomeWork;
+
 import java.util.*;
+
 public class Exercise13 {
     public static void main(String[] args) {
         int[][] array = new int[][]{
@@ -13,13 +15,29 @@ public class Exercise13 {
                 {0, 39, 24, 1, 18}
         };
         for (int i = 0; i < array.length; i++) {
-            for (int j = 0; j < array.length; j++) {
-                var tmp = array[i][array.length-5];
-                array[i][array.length-5] = array[i][array.length-1];
-                array[i][array.length-1] = tmp;
+            for (int j = 0; j < array[i].length; j++) {
                 System.out.print(array[i][j] + "\t");
             }
             System.out.println();
         }
+        System.out.print("\n");
+        int n, m, temp;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Введите номер первого столбца для замены от 0 до 4:  ");
+        n = in.nextInt();
+        System.out.println("Введите номер второго столбца для замены от 0 до 4:  ");
+        m = in.nextInt();
+        if (n > array.length && m > array.length) ;
+        System.out.println("Введено неверное значение");
+        for (int i = 0; i < array.length; i++) {
+            temp = array[i][n];
+            array[i][n] = array[i][m];
+            array[i][m] = temp;
+            System.out.println(" Массив с измененным " + n + " столбцом  на " + m + " столбец ." + "\t");
+            for (int j = 0; j < array[i].length; j++) {
+                System.out.print(array[i][j] + "\t");
+            }
+        }
+        System.out.println();
     }
 }
